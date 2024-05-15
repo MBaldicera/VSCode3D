f80a53ce00b8cb906b4b3bfb60679cee64a6aea0
MBaldicera <126473999+MBaldicera@users.noreply.github.com>
2024-05-15 19:49:51 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
-const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-05-10T00:00:00");
-
-contadores[0].textContent = tempoObjetivo1;
 
 for (let i=0; i<botoes.length;i++){
     botoes[i].onclick=function(){
@@ -14,4 +10,17 @@ for (let i=0; i<botoes.length;i++){
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
+}
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-05-20T00:00:00");
+
+function calculaTempo(tempoObjetivo){
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+    return dias;
 }
